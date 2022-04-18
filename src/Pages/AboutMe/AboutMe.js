4dfc94da9da12c7f6bade894ef9170b89800cc00
Hridoyshom <hshom827@gmail.com>
2022-04-18 20:50:14 +0600

    abou me updated

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <div>
-            <h1>heeieiiei</h1>
+        <div style={{ width: '50%' }} >
+            <h2>My name is Hridoy Shom</h2>
+            <p>
+                In future I want to be a full stack web developer. I am already trying hard to become a junior web developer. I think practice and right guidelines can make my dream possible. From my side i am trying my best to be the best developer. I think if i continue like the for couple of years i will be successful by the grace of Almighty.
+            </p>
+
         </div>
     );
 };
